6fa0542e38c14682ab1ab703fcadee7472deaf04
Aziz Mannanov <azizmannanov@hotmail.co.uk>
2022-11-02 02:05:02 +0100

    Add project files.

---
+++ src/Boat Parts/main.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+bool check(list<string> partList, string part) {
+    list<string>::iterator uit;
+
+    for (uit = partList.begin();uit != partList.end();uit++) {
+        if (*uit == part) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int P, N;
+
+    int day = 0, lastDay = 0;
+    cin >> P >> N;
+    string part;
+    list<string> parts, uniqueParts;
+    list<string>::iterator it, uit;
+
+    for (int i = 0;i < N;i++) {
+        cin >> part;
+        parts.push_back(part);
+    }
+    for (it = parts.begin();it != parts.end(); ++it) {
+        day++;
+        if (!check(uniqueParts, *it)) {
+            uniqueParts.push_back(*it);
+            lastDay = day;
+        }
+    }
+    if (uniqueParts.size() < P) {
+        cout << "paradox avoided";
+    }
+    else {
+        cout << lastDay;
+    }
+    return 0;
+}
---
+++ src/Tram/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int a = 0;
+
+    cin >> n;
+    int* xy = new int[2 * n];
+
+    for (int i = 0;i < n;i++) {
+        cin >> *(xy + 2 * i) >> *(xy + 2 * i + 1);
+    }
+    for (int i = 0;i < n;i++) {
+        a += *(xy + 2 * i + 1) - *(xy + 2 * i);
+    }
+
+    cout << (float)a / (float)n;
+    return 0;
+}
